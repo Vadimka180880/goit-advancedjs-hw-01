f1f6223ae180d0aa81bda39ff96ea61e2d74b587
Vadimka180880 <vadim.te180880@gmail.com>
2024-12-07 08:17:19 -0500

    Updated files for proper deployment

--- vite.config.js
+++ vite.config.js
@@ -1,52 +1,26 @@
 import { defineConfig } from 'vite';
-import { glob } from 'glob';
-import injectHTML from 'vite-plugin-html-inject';
-import FullReload from 'vite-plugin-full-reload';
-import SortCss from 'postcss-sort-media-queries';
 import path from 'path';
 
-export default defineConfig(({ command }) => {
-  return {
-    define: {
-      global: {}, 
-    },
-    root: 'src', 
-    publicDir: 'public', 
-    build: {
-      outDir: '../dist',
-      sourcemap: true,
-      rollupOptions: {
-        input: glob.sync('./public/*.html').reduce((entries, file) => {
-          const fileName = path.basename(file, '.html');
-          entries[fileName] = path.resolve(__dirname, file);
-          return entries;
-        }, {}),
-        output: {
-          manualChunks(id) {
-            if (id.includes('node_modules')) {
-              return 'vendor';
-            }
-          },
-          assetFileNames: 'assets/[name]-[hash][extname]',
-        },
-      },
-      emptyOutDir: true,
-    },
-    css: {
-      postcss: {
-        plugins: [
-          SortCss({
-            sort: 'mobile-first',
-          }),
-        ],
+export default defineConfig(() => ({
+  base: '/goit-advancedjs-hw-01/', 
+  root: 'src',
+  publicDir: 'public',
+  define: {
+    global: 'window', 
+  },
+  build: {
+    outDir: '../dist',
+    sourcemap: true,
+    emptyOutDir: true,
+    rollupOptions: {
+      input: {
+        main: path.resolve(__dirname, 'src/public/index.html'),
+        gallery: path.resolve(__dirname, 'src/public/1-gallery.html'),
+        form: path.resolve(__dirname, 'src/public/2-form.html'),
       },
     },
-    plugins: [
-      injectHTML(),
-      FullReload(['./public/**/*.html']),
-    ],
-    server: {
-      open: '/public/index.html', 
-    },
-  };
-});
+  },
+  server: {
+    open: '/public/index.html',
+  },
+}));
